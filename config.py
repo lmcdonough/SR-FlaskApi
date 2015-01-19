f7d0cb35a2b0001c2273d4f2ae95803711b3f245
lmcdonough <levimcdonough@gmail.com>
2015-01-19 23:59:29 +0000

    updated config to remove commented code

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ SECRET_KEY = 'you-will-never-guess'
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'mysql://levi:python@localhost/simpledb'
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
